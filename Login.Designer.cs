d060118c0a9c61e3f0eaeccbd5652c559be07762
Jai <47360581+jaidude@users.noreply.github.com>
2019-05-21 16:05:38 +0530

    Add files via upload

---
+++ Login.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace BR
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightGray;
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Location = new System.Drawing.Point(159, 159);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightGray;
+            this.label2.Font = new System.Drawing.Font("Lucida Fax", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label2.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Location = new System.Drawing.Point(164, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.SystemColors.Control;
+            this.linkLabel1.Font = new System.Drawing.Font("Lucida Fax", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.linkLabel1.LinkColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.linkLabel1.Location = new System.Drawing.Point(100, 278);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(147, 17);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Forgot Password?";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.BackColor = System.Drawing.SystemColors.Control;
+            this.linkLabel2.Font = new System.Drawing.Font("Lucida Fax", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.linkLabel2.LinkColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.linkLabel2.Location = new System.Drawing.Point(287, 278);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(180, 17);
+            this.linkLabel2.TabIndex = 3;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Create a new Account";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Lucida Fax", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button1.Location = new System.Drawing.Point(172, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(268, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(268, 198);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LightGray;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label3.Location = new System.Drawing.Point(205, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 43);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Login Page";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LightGray;
+            this.label4.Font = new System.Drawing.Font("Lucida Fax", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label4.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.Location = new System.Drawing.Point(145, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Designation";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Staff",
+            "Customer"});
+            this.comboBox4.Location = new System.Drawing.Point(268, 236);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(100, 21);
+            this.comboBox4.TabIndex = 26;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Lucida Fax", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button2.Location = new System.Drawing.Point(310, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe WP Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label5.Location = new System.Drawing.Point(122, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(345, 32);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "BUS RESERVATION SYSTEM";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(599, 398);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Login.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Oracle.DataAccess.Client;
+
+namespace BR
+{
+    public partial class Login : Form
+    {
+       public static string getemail,getname;
+         OracleConnection CON1 = new OracleConnection();
+         OracleCommand CMD;
+       string QUERY;
+        //int TEMP;
+         OracleDataReader RDR;
+
+        public Login()
+        {
+            InitializeComponent();
+            CON1 = new OracleConnection("Data Source=XE;User ID=project;password=project ");
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            CON1 = new OracleConnection("Data Source=XE;User ID=project;password=project ");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel2.BackColor = System.Drawing.Color.Transparent;
+
+            comboBox4.Text = "--SELECT--";
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == " ") || (textBox2.Text == "") || (comboBox4.Text == "") || (comboBox4.Text == "--SELECT--"))
+            {
+                MessageBox.Show("Enter the all the details");
+            }
+            // for staff 
+            else
+            {
+                
+            if (comboBox4.SelectedItem.ToString() == "Staff")
+                {
+                    int FLAG = 0;
+                    string STR = "";
+
+                    QUERY = "select * from S_CREATE_ACCOUNT where username ='" + textBox1.Text + "' ";
+                    CMD = new OracleCommand(QUERY, CON1);
+
+                    CON1.Open();
+                    RDR = CMD.ExecuteReader();
+                    while (RDR.Read())
+                    {
+                        STR = (string)RDR["password"];
+                        FLAG = 1;
+                    }
+                    RDR.Close();
+
+                    CON1.Close();
+                    if (FLAG == 0)
+                    {
+                        MessageBox.Show(" username doesnt exist", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        clearform();
+
+                    }
+                    else if (textBox2.Text == STR)
+                    {
+                       
+                                 staffcontrol op = new staffcontrol();
+                                 op.Show();
+                                 Hide();
+                        
+
+                        Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username and Password doesnt match.Please enter correct username and Password", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBox2.Text = " ";
+                        textBox2.Focus();
+                    }
+                }
+
+
+                // for customer
+                else
+                {
+                    int FLAG1 = 0;
+                    string STR1 = "";
+
+                    QUERY = "select * from C_CREATE_ACCOUNT where username ='" + textBox1.Text + "' ";
+                    CMD = new OracleCommand(QUERY, CON1);
+
+                    CON1.Open();
+                    RDR = CMD.ExecuteReader();
+                    while (RDR.Read())
+                    {
+                        STR1 = (string)RDR["password"];
+                        FLAG1 = 1;
+                    }
+                    RDR.Close();
+
+                    CON1.Close();
+                    if (FLAG1 == 0)
+                    {
+                        MessageBox.Show(" username doesnt exist", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        clearform();
+
+                    }
+                    else if (textBox2.Text == STR1)
+                    {
+                                //fatch the email adderss of the user
+
+                     QUERY = "select email from C_CREATE_ACCOUNT where username ='" + textBox1.Text + "' ";
+                    CMD = new OracleCommand(QUERY, CON1);
+                        getname = textBox1.Text;
+                    CON1.Open();
+                    RDR = CMD.ExecuteReader();
+                    while (RDR.Read())
+                    {
+                        getemail = (string)RDR["email"];                       
+                    }
+                    RDR.Close();
+
+                    CON1.Close();
+                        //value passed to the constructor of another form
+                     //   MessageBox.Show(getemail);
+                      //  Booking m = new Booking(getemail);
+                                 customercontrol op = new customercontrol();
+                                 op.Show();
+                                 Hide();
+                           
+
+                        Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Username and Password doesnt match.Please enter correct username and Password", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        textBox2.Text = " ";
+                        textBox2.Focus();
+                    }
+
+                }
+            }
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Forgot_password op = new Forgot_password();
+            op.Show();
+            Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        void clearform()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox1.Focus();
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            createstaff op = new createstaff();
+            op.Show();
+            Hide();
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
